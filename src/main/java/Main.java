5f41f6e3e720c854cc7a36e2ff83f57d3cbf1eb8
Rizat19 <e-rizat.orazalina@homecredit.kz>
2021-12-06 12:23:13 +0600

    lesson three. homework

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/*
+ *    Rizat.Orazalina created on 06.12.2021
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Lesson Three. HomeWork");
+    }
+}
