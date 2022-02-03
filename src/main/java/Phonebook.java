a2a93ff891ebf9a1384297745adf83d94a83c7b4
rizatrio <90304244+rizatrio@users.noreply.github.com>
2022-02-03 11:27:54 +0600

    Update Phonebook.java

--- src/main/java/Phonebook.java
+++ src/main/java/Phonebook.java
@@ -6,34 +6,19 @@ import java.util.*;
 
 public class Phonebook {
 
-    private HashMap<String, List<String>> phoneBook;
+    private HashMap<String, List<String>> list = new HashMap<>();
 
-    public Phonebook() {
-        this.phoneBook = new HashMap<>();
-    }
-
-    public void add(String surname, String phoneNumber) {
-        if (phoneBook.containsKey(surname)) {
-            List<String> phoneNumbers = phoneBook.get(surname);
-            if (!phoneNumbers.contains(phoneNumber)) {
-                phoneNumbers.add(phoneNumber);
-                System.out.printf("Телефон номер %s добавлен для фамилии %s", phoneNumber, surname);
-            } else {
-                System.out.printf("Телефон номер %s уже существует для фамилии %s", phoneNumber, surname);
+    public void add(String surname, String number) {
+        if (list.containsKey(surname)) {
+            if (!list.get(surname).contains(number)) {
+                list.get(surname).add(number);
             }
         } else {
-            phoneBook.put(surname, new ArrayList<>(Arrays.asList(phoneNumber)));
-            System.out.printf("Номер %s добавлен для фамилии %s", phoneNumber, surname);
+            list.put(surname, new ArrayList<>(Collections.singletonList(number)));
         }
     }
 
     public List<String> get(String surname) {
-        if (phoneBook.containsKey(surname)) {
-            System.out.printf("В телефонном справочнике есть запись для фамилии %s ", surname);
-            return phoneBook.get(surname);
-        } else {
-            System.out.printf("В телефонном справочнике нет записи для фамилии %s", surname);
-            return new ArrayList<>();
-        }
+        return  list.get(surname);
     }
 }
